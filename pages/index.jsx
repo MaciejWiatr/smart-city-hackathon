9e3f9e557a66f0d85a029347705f5eee6ca36a29
Kacper Szot <kacperszot1@gmail.com>
2021-11-20 15:55:01 +0100

    move components dir to the src

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,14 @@
 import { Input } from '@chakra-ui/input'
-import { Heading, Stack } from '@chakra-ui/layout'
+import { Heading, Stack, Center } from '@chakra-ui/react'
 import Head from 'next/head'
 import Image from 'next/image'
-import { Header } from '../components/header.tsx'
+import { Header } from '../src/components/header.tsx'
+import theme from '../src/theme'
 
 export default function Home() {
   return (
     <Stack>
-      <Header>
+      <Header bgImage="bg1.png">
         <Heading
           pb="3"
           maxWidth="600"
@@ -28,6 +29,21 @@ export default function Home() {
           bgColor="white"
         ></Input>
       </Header>
+      <Center pt="2">
+        <Stack>
+          <Heading
+            width="60vw"
+            maxWidth="600"
+            fontWeight="600"
+            pt="5"
+            pb="5"
+            textAlign="left"
+            color={theme.textGrey}
+          >
+            Gdzie jesteś? 🤔
+          </Heading>
+        </Stack>
+      </Center>
     </Stack>
   )
 }
